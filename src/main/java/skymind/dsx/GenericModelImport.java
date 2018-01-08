bdc3c029f58a212c28f9459904945a014f1c1856
tomthetrainer <tom@functionalmedia.com>
2018-01-07 23:56:35 -0500

    new class for lab

---
+++ src/main/java/skymind/dsx/GenericModelImport.java
@@ -0,0 +1,32 @@
+package skymind.dsx;
+
+/**
+ * Created by tomhanlon on 12/29/17.
+ */
+import org.apache.log4j.BasicConfigurator;
+import org.deeplearning4j.nn.modelimport.keras.KerasModelImport;
+import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.factory.Nd4j;
+
+
+public class GenericModelImport {
+    public static void main(String[] args) throws Exception{
+        BasicConfigurator.configure();
+
+
+
+        String kerasModelfromKerasExport = "Keras_export_full_iris_model";
+
+
+        MultiLayerNetwork model = KerasModelImport.importKerasSequentialModelAndWeights(kerasModelfromKerasExport);
+
+
+        System.out.println("Model Configuration");
+        String modelconfig = model.conf().toJson();
+        System.out.println(modelconfig);
+
+
+
+    }
+}
